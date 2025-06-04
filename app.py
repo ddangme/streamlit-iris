6b3b1abc2bb29b22e7b77aa899702f6102204b07
ddangme <ssilver121@naver.com>
2025-06-04 18:00:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title(" ☆ 꽃 분 류 기 ( I r i s C l a s s i f i e r ) ")
 st.write(" 입 력 값 을 기 반 으 로 꽃 의 종 류 를 예 측 합 니 다 . ")
 
 # 사 용 자 입 력 받 기
-sepal_length = st.slider("Sepal Length (cm)", 4.0 8.0, 5.1)
+sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0, 5.1)
 sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.5, 3.5)
 petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
 petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.2)
